eb3d50a0a5f89cceaa835a7d78133b8c0e6c5404
wafiter <wafiter@gmail.com>
2024-05-15 22:19:12 -0300

    save

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+import { Conteiner } from "../../components/baselayot/style";
+import { ConteinerWhithe } from "../login/style";
+
+const Home = () => {
+  return (
+    <>
+      <Conteiner>
+        <div></div>
+      </Conteiner>
+      <ConteinerWhithe>
+        <Conteiner></Conteiner>
+      </ConteinerWhithe>
+    </>
+  );
+};
+
+export default Home
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPag from "../pages/login";
 import Baselayout from "../components/baselayot/baseLayout";
 import Cadastro from "../pages/cadastro";
+import Home from "../pages/home";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route element={<Baselayout />} path="/">
+      <Route element={<Baselayout />} >
+        <Route element={<Home />} path="/" />
         <Route element={<LoginPag />} path="/login" />
         <Route element={<Cadastro />} path="/cadastro" />
       </Route>
